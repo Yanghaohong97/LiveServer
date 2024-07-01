23ede5c41f254b375a1b68f1994f94bf8efb80ac
yanghaohong <yanghaohong@cvte.com>
2024-07-01 15:46:30 +0800

    实现一个高性能的HTTP-FLV流媒体服务器

---
+++ HpHttpFlvServer/main.cpp
@@ -0,0 +1,31 @@
+﻿#include "Server/BoostServer.h"
+#include "Scheduler.h"
+#include "Utils/Config.h"
+
+/*
+ffmpeg 播放http-flv视频流
+    ffplay -i http://localhost:8080/test.flv
+
+*/
+
+int main(int argc, char* argv[])
+{
+    const char* file = NULL;
+    file = "config.json";
+
+    Config config(file);
+    if (!config.state) {
+        printf("failed to read config file: %s\n", file);
+        return -1;
+    }
+    
+    BoostServer server(&config);
+    std::thread([&]() {
+        server.start();
+    }).detach();
+    
+    Scheduler sch(&server,&config);
+    sch.loop();
+
+    return 0;
+}
